d18fc64ab85758865da004191b0f7c45cd45843c
Isaac Nguyen <isaacnguyen03@gmail.com>
2023-01-21 04:47:24 -0500

    no async

--- scrape.py
+++ scrape.py
@@ -13,17 +13,36 @@ class Data:
         self.easy = easy
         self.liked = liked
 
-def 
-r = session.get(url)
+    def print(self, counter):
+        print("#" + str(counter + 1))
+        print("Course: " + str(self.course))
+        print("Name: " + str(self.name))
+        print("Ratings: " + str(self.ratings))
+        print("Useful: " + str(self.useful))
+        print("Easy: " + str(self.easy))
+        print("Liked: " + str(self.liked))
 
-r.html.render(sleep=1, keep_page=True, scrolldown=1) #654
 
-rows = r.html.find("div[role='row']")
+def scrape():
+    r = session.get(url)
+    r.html.render(sleep=1, keep_page=True, scrolldown=114) #654
 
-rows.pop(0)
+    rows = r.html.find("div[role='row']")
 
-for i, item in enumerate(rows):
-    cells = item.find("div[role='cell']") #array of each cells
-    data = {
+    rows.pop(0)
+    return rows
+
+def print_data(rows):
+    for i, item in enumerate(rows):
+        cells = item.find("div[role='cell']") #array of each cells
+        data = Data(cells[0].text, cells[1].text, int(cells[2].text), int(cells[3].text[:-1]), int(cells[4].text[:-1]), int(cells[5].text[:-1]))
+
+        data.print(i)
+        print()
         
-    }
+
+if __name__ == '__main__':
+    print("Scraping..")
+    rows = scrape()
+
+    print_data(rows)
